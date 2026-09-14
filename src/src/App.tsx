1fa0f6ac6cc0304a0a24faa4f9765a33dfc09046
JyMMerk2 <jymmerk2@gmail.com>
2026-09-14 10:47:47 -0400

    Create App component for financial management

---
+++ src/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Gestión Financiera</h1>
+      <p>Cargando aplicación modular...</p>
+    </div>
+  );
+}
+
+export default App;
